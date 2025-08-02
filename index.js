22216e63c0d832b01e91521023693b261ae80c93
vigan-abd <vigan.abd@gmail.com>
2025-08-02 02:12:43 +0200

    dep update and corestore opts

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ class StoreFacility extends Base {
         }
 
         this.store = new Corestore(this.opts.storeDir, {
+          ...(this.opts.corestoreOpts ?? {}),
           primaryKey: this.opts.storePrimaryKey
             ? Buffer.from(this.opts.storePrimaryKey, 'hex')
             : null
